718693607c251d38a5b95a49a7b37e3e7b9dfbae
patelmeet341993 <patelmeet341993@gmail.com>
2019-07-29 14:34:12 +0530

    ss
    Image Chooser

---
+++ app/src/main/java/friendlyitsolution/com/filechooser/MainActivity.java
@@ -0,0 +1,126 @@
+package friendlyitsolution.com.filechooser;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
+
+   static ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        img = findViewById(R.id.img);
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImage();
+            }
+        });
+    }
+
+
+    void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select image for IMG"), 1);
+
+    }
+
+    void chooseSong() {
+        Intent intent = new Intent();
+        intent.setType("audio/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select image for IMG"), 2);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data.getData() != null) {
+
+            Uri uri = data.getData();
+          // setIMG(uri);
+
+            try {
+                Bitmap bits = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+                img.setImageBitmap(bits);
+
+            } catch (Exception e) {
+
+            }
+
+        } else if (requestCode == 2 && resultCode == RESULT_OK && data.getData() != null) {
+
+            Uri u = data.getData();
+
+
+
+            Toast.makeText(getApplicationContext(), "selected file :" + u.toString(), Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "select any file", Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+  /*  static void setIMG(Uri uri)
+    {
+        try {
+            Bitmap bits = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+            img.setImageBitmap(bits);
+
+        } catch (Exception e) {
+
+        }
+    }
+*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.ic) {
+            chooseImage();
+        } else if (id == R.id.sc) {
+            chooseSong();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
